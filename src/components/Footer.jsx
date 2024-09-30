ea81deb70177eaace276bdca371954c8d7c995f7
[AgheraRaj] <[rajaghara2004@gmail.com]clear>
2024-09-30 17:28:46 +0530

    Push

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6">
+      <p className="text-xs text-muted-foreground">
+        &copy; 2024 Raj Aghera. All rights reserved.
+      </p>
+      <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <div className="text-xs hover:underline underline-offset-4">
+          Privacy
+        </div>
+        <div className="text-xs hover:underline underline-offset-4">Terms</div>
+      </nav>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
